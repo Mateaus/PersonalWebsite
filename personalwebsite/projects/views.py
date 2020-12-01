c41e834d59b9ccdd870b101595a27d634d909b95
Anthony9698 <anthonyviera4@gmail.com>
2020-12-01 01:48:27 -0600

    Passed dummy data to projects page to display multiple projects.

--- personalwebsite/projects/views.py
+++ personalwebsite/projects/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def projects(request):
-    return render(request, 'project/projects.html')
+    project_list = ['Project 1', 'Project 2', 'Project 3']
+    context = {
+        'project_list': project_list,
+    }
+    return render(request, 'project/projects.html', context)
