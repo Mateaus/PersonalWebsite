96e860db996cfe84da24bd255b83495833521917
Mateaus <mateausc@gmail.com>
2020-12-03 17:27:22 -0600

    Added model for sections and section contents

--- personalwebsite/aboutme/models.py
+++ personalwebsite/aboutme/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Section(models.Model):
+    title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.title
+
+class SectionContent(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    content = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title + " - " + self.content
