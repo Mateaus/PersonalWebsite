4a34116c3dc7cc3d42742bc413b244b498582e4d
Mateaus <mateausc@gmail.com>
2020-12-03 21:26:44 -0600

    Removed permission for admin if an introduction already exists.

--- personalwebsite/aboutme/admin.py
+++ personalwebsite/aboutme/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Introduction, Section, SectionContent
 
-admin.site.register(Introduction)
 admin.site.register(Section)
 admin.site.register(SectionContent)
+
+@admin.register(Introduction)
+class IntroductionModelAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        """ 
+        Allows the admin to only add a single entry for Introduction.
+        """
+        count = Introduction.objects.all().count()
+        if count == 0:
+            return True
+        return False
