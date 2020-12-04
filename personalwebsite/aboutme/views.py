0e7bac138193dd17f0c14b88cbc41fb912a10265
Mateaus <mateausc@gmail.com>
2020-12-03 18:08:20 -0600

    Added built in template to dynamically load content from the db.

--- personalwebsite/aboutme/views.py
+++ personalwebsite/aboutme/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Section, SectionContent
 
 # Create your views here.
 def about_me(request):
-    return render(request, 'aboutme/about_me_page_view.html')
+    sections = Section.objects.all()
+    section_contents = SectionContent.objects.all()
+
+    context = {
+        "sections": sections,
+        "contents": section_contents
+    }
+
+    return render(request, 'aboutme/about_me_page_view.html', context=context)
