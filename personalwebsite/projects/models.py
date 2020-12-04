98dd2eff5a196b012b7fab800581cff3321d2ad0
Anthony9698 <anthonyviera4@gmail.com>
2020-12-04 00:04:35 -0600

    Set up basic model for projects.

--- personalwebsite/projects/models.py
+++ personalwebsite/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from django.utils.timezone import now
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=64, null=False)
+    description = models.TextField()
+    data_added = models.DateTimeField(default=now, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True)
