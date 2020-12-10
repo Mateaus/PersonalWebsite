31c47c29222e6f3df910c2f0ff71daf3d1ad8561
Anthony9698 <anthonyviera4@gmail.com>
2020-12-10 00:56:08 -0600

    Added a model for project tags.

--- personalwebsite/projects/models.py
+++ personalwebsite/projects/models.py
@@ -3,6 +3,17 @@ from datetime import datetime
 from django.utils.timezone import now
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+    icon = models.ImageField(
+        upload_to="project/tag/icons/",
+        height_field=None,
+        width_field=None,
+        blank=True,
+        null=True
+    )
+
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=64, null=False)
@@ -14,5 +25,6 @@ class Project(models.Model):
         blank=True,
         null=True
     )
+    tags = models.ManyToManyField(Tag)
     data_added = models.DateTimeField(default=now, editable=False)
     last_modified_date = models.DateTimeField(auto_now=True)
