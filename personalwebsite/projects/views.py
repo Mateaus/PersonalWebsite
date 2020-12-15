ab46adfe11e63420ac924570913e32bc28940e59
Anthony9698 <anthonyviera4@gmail.com>
2020-12-15 14:44:03 -0600

    Added paging to projects page and added media queries to add more top margin to tags when resizing screen.

--- personalwebsite/projects/views.py
+++ personalwebsite/projects/views.py
@@ -5,7 +5,18 @@ from .models import Project
 # Create your views here.
 def projects(request):
     all_projects = Project.objects.all()
+    paginator = Paginator(all_projects, 3)
+    page = request.GET.get('page')
+
+    try:
+        project_list = paginator.page(page)
+    except PageNotAnInteger:
+        project_list = paginator.page(1)
+    except EmptyPage:
+        project_list = paginator.page(paginator.num_pages)
+
     context = {
-        'all_projects': all_projects
+        'all_projects': all_projects,
+        'project_list': project_list
     }
     return render(request, 'projects.html', context)
