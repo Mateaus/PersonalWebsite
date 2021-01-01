f16dc0e3903d8b4a8e4a78b1ba5796642f460c1a
Mateaus <mateausc@gmail.com>
2021-01-01 07:12:26 -0600

    Cleared unused imports.

--- personalwebsite/projects/models.py
+++ personalwebsite/projects/models.py
@@ -1,31 +1,29 @@
 from django.db import models
-from datetime import datetime
-from django.utils.timezone import now
-from django.core.validators import FileExtensionValidator
-from django.core.exceptions import ValidationError
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=32, null=False, blank=False)
-    data_added = models.DateTimeField(default=now, editable=False)
+    data_added = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
-    
+
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=64, null=False)
     description = models.TextField()
     cover_image = models.ImageField(
-        upload_to="project/cover_images/", 
-        height_field=None, 
-        width_field=None, 
+        upload_to="project/cover_images/",
+        height_field=None,
+        width_field=None,
         blank=True,
         null=True
     )
     tags = models.ManyToManyField(Tag, blank=True)
-    data_added = models.DateTimeField(default=now, editable=False)
+    data_added = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
