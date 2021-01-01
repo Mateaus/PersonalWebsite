53e6430f7a3bc29326cd311d7c30fda0a292dd0e
Mateaus <mateausc@gmail.com>
2021-01-01 14:06:34 -0600

    Added default tags for all pages.

--- personalwebsite/tag/templatetags/tags.py
+++ personalwebsite/tag/templatetags/tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 def show_tags(model):
     tag_list = model.tags.all()
     return {'tags': tag_list}
+
+@register.inclusion_tag('tag/tag_css.html')
+def default_tag_css():
+    # Returns the html containing default css for tags
+    return
