353a0cb2ad94bb0ac902e4c1176bc04de3623a75
Anthony9698 <anthonyviera4@gmail.com>
2021-01-05 14:19:36 -0600

    Migrated project detail close button function from onclick to script file.

--- personalwebsite/static/projects/dist/js/projects.js
+++ personalwebsite/static/projects/dist/js/projects.js
@@ -10,10 +10,10 @@ function displayProjectDetail(element) {
         if (xhttp.readyState == XMLHttpRequest.DONE) {
             if (xhttp.status == 200) {
                 document.getElementById("project-detail").innerHTML = xhttp.responseText;
-            } 
+            }
             else if (xhttp.status == 400) {
                 alert("There was an error 400");
-            } 
+            }
             else {
                 alert("Something else other than 200 was returned");
             }
@@ -23,8 +23,30 @@ function displayProjectDetail(element) {
     xhttp.send();
 }
 
+const learnMoreElements = document.getElementsByClassName('learn-more');
+Array.from(learnMoreElements).forEach(element => {
+    element.addEventListener('click', () => displayProjectDetail(element));
+});
+
+const projectDetailDiv = document.getElementById("project-detail");
+projectDetailDiv.addEventListener('click', event => {
+    // button was pressed inside detail div
+    if (event.target && event.target.nodeName == "BUTTON") {
+        // get button classes
+        const classes = event.target.className.split(" ");
+        // make sure classes of the button matches close button classes
+        if (classes) {
+            const targetClasses = JSON.stringify(["btn", "close-project"]);
+            const selectedBtnClasses = JSON.stringify(classes);
+            if (targetClasses === selectedBtnClasses) {
+                closeProjectDetail(event.target);
+            }
+        }
+    }
+});
+
 // close project details when clicking outside div
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     let projectDetail = document.getElementById('project-detail');
     let isClickInside = projectDetail.contains(event.target);
 
