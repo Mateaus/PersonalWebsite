595221e6a32bc9011ed8fd6962c4bd63336dc649
Anthony9698 <anthonyviera4@gmail.com>
2021-01-05 03:04:37 -0600

    Project details box is now closed when clicking outside of the div.

--- personalwebsite/static/projects/dist/js/projects.js
+++ personalwebsite/static/projects/dist/js/projects.js
@@ -1,4 +1,34 @@
-function closeProjectDetail(closeBtn) {
-    const projectDetailDiv = closeBtn.parentNode.parentNode;
+function closeProjectDetail(element) {
+    const projectDetailDiv = element.parentNode.parentNode;
     projectDetailDiv.parentNode.removeChild(projectDetailDiv);
 }
+
+function displayProjectDetail(element) {
+    let projectId = element.parentElement.parentElement.id;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == XMLHttpRequest.DONE) {
+            if (xhttp.status == 200) {
+                document.getElementById("project-detail").innerHTML = xhttp.responseText;
+            } 
+            else if (xhttp.status == 400) {
+                alert("There was an error 400");
+            } 
+            else {
+                alert("Something else other than 200 was returned");
+            }
+        }
+    }
+    xhttp.open("GET", "detail/" + projectId, true);
+    xhttp.send();
+}
+
+// close project details when clicking outside div
+document.addEventListener('click', function(event) {
+    let projectDetail = document.getElementById('project-detail');
+    let isClickInside = projectDetail.contains(event.target);
+
+    if (!isClickInside) {
+        projectDetail.innerHTML = "";
+    }
+});
