3d79b651d1d388ebcc5965d1d347d473b52b9a2e
Anthony9698 <anthonyviera4@gmail.com>
2021-01-05 19:54:11 -0600

    Outsourced function to execute a callback function if targeted element matches targeted classes.

--- personalwebsite/static/projects/dist/js/projects.js
+++ personalwebsite/static/projects/dist/js/projects.js
@@ -1,6 +1,6 @@
 function closeProjectDetail(element) {
     const projectDetailDiv = element.parentNode.parentNode;
-    projectDetailDiv.parentNode.removeChild(projectDetailDiv);
+    projectDetailDiv.innerHTML = "";
 }
 
 function displayProjectDetail(element) {
@@ -9,12 +9,12 @@ function displayProjectDetail(element) {
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == XMLHttpRequest.DONE) {
             if (xhttp.status == 200) {
-                document.getElementById("project-detail").innerHTML = xhttp.responseText;
-            }
-            else if (xhttp.status == 400) {
+                let projectDetailDiv = document.getElementById("project-detail");
+                projectDetailDiv.innerHTML = xhttp.responseText;
+                projectDetailDiv.classList.add("detail");
+            } else if (xhttp.status == 400) {
                 alert("There was an error 400");
-            }
-            else {
+            } else {
                 alert("Something else other than 200 was returned");
             }
         }
@@ -23,45 +23,35 @@ function displayProjectDetail(element) {
     xhttp.send();
 }
 
-const mainProjDiv = document.getElementsByClassName("project-main")[0];
-mainProjDiv.addEventListener('click', event => {
-    if (event.target && event.target.nodeName == "BUTTON") {
-        let classes = event.target.className.split(" ");
-        if (classes) {
-            let targetClasses = JSON.stringify(["learn-more"]);
-            let selectedBtnClasses = JSON.stringify(classes);
-            if (targetClasses === selectedBtnClasses) {
-                displayProjectDetail(event.target);
-            }
-        }
-    }
-});
-
-
 // Detecting click event here using DOM event delegation
-const projectDetailDiv = document.getElementById("project-detail");
-projectDetailDiv.addEventListener('click', event => {
-    // button was pressed inside detail div
-    if (event.target && event.target.nodeName == "BUTTON") {
-        // get button classes
-        let classes = event.target.className.split(" ");
-        // make sure classes of the button matches close button classes
+function executeFuncOnClass(e, targetElType, targetClassesArr, callBackFunc) {
+    if (e.target && e.target.nodeName === targetElType) {
+        let classes = e.target.className.split(" ");
         if (classes) {
-            let targetClasses = JSON.stringify(["btn", "close-project"]);
-            let selectedBtnClasses = JSON.stringify(classes);
-            if (targetClasses === selectedBtnClasses) {
-                closeProjectDetail(event.target);
+            let targetClasses = JSON.stringify(targetClassesArr);
+            let selectedElClasses = JSON.stringify(classes);
+            if (targetClasses === selectedElClasses) {
+                callBackFunc(e.target);
             }
         }
     }
-});
+}
+
+const mainProjDiv = document.getElementsByClassName("project-main")[0];
+mainProjDiv.addEventListener('click', event =>
+    executeFuncOnClass(event, "BUTTON", ["learn-more"], displayProjectDetail));
+
+const projectDetailDiv = document.getElementById("project-detail");
+projectDetailDiv.addEventListener('click', event =>
+    executeFuncOnClass(event, "BUTTON", ["btn", "close-project"], closeProjectDetail));
 
 // close project details when clicking outside div
-document.addEventListener('click', function (event) {
-    let projectDetail = document.getElementById('project-detail');
-    let isClickInside = projectDetail.contains(event.target);
+document.addEventListener('click', event => {
+    let projectDetailDiv = document.getElementById('project-detail');
+    let isClickInside = projectDetailDiv.contains(event.target);
 
     if (!isClickInside) {
-        projectDetail.innerHTML = "";
+        projectDetailDiv.innerHTML = "";
+        projectDetailDiv.classList.remove("detail");
     }
 });
