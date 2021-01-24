01f084b0e27604af3c18bbba2f470faa2d9db2e1
Anthony9698 <anthonyviera4@gmail.com>
2021-01-24 01:38:24 -0600

    Added multiple image capability to all projects through use of a image album and image model.

--- personalwebsite/projects/admin.py
+++ personalwebsite/projects/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django import forms
-from .models import Project
+from .models import Project, Image, ImageAlbum
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 
 class ProjectForm(forms.ModelForm):
@@ -18,7 +20,35 @@ class ProjectForm(forms.ModelForm):
         return cleaned_data
 
 
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['name', 'image', 'default', 'album']
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     form = ProjectForm
     filter_horizontal = ('tags',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    form = ImageForm
+
+
+@admin.register(ImageAlbum)
+class ImageAlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'default')
+    readonly_fields = ('images',)    
+
+    def default(self, album):
+        return format_html('<img src="{}" style="width: 150px; height:100px;" />'.format(album.images.filter(default=True).first().image.url))
+
+    def images(self, album):
+        album_images = []
+        for obj in album.images.all():
+            album_images.append('<img src="{}" style="width: 150px; height:100px; margin-right: 10px" />'.format(obj.image.url))
+        album_images = "".join(album_images)
+        return format_html(album_images)
+
